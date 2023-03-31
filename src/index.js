26e1eacfa3dc3c043316cb5258f9676f16500dab
melman_artem <melman69rus@mail.ru>
2023-03-31 23:10:50 +0300

    add changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import'normalize.css'
-import './styles/main.scss'
+import'normalize.css';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import './styles/main.scss';
 
 import Swiper, { Navigation, Pagination } from 'swiper';
 
